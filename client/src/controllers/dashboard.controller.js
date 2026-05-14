6b18a8c490aa533f5562a8f50891e0bf63d1b598
akki110 <128359423+akki110@users.noreply.github.com>
2026-05-15 00:51:25 +0530

    remove unnecessary code

--- client/src/controllers/dashboard.controller.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = require('../middlewares/handler.middleware.js');
-const ApiResponse = require('../utils/ApiResponse.js');
-const dashboardService = require('../services/dashboard.service.js');
-
-exports.getDashboardData = asyncHandler(async (req, res) => {
-    const userId = req.user.id;
-    const result = await dashboardService.getDashboardData(userId);
-    res.status(200).json(new ApiResponse(200, result, "Dashboard data fetched successfully"));
-});
